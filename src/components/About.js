f2993d4aac588d556fcca03d29777c31f4d73202
Haley May <haymon24u@gmail.com>
2023-01-06 12:43:49 -1000

    creates all components needed

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import profileImage from "../../assets/large/Profile/Profile_Pic.png";
+function About() {
+  return (
+    <section id="about-me" className="my-5 intro">
+      <h1>About Me</h1>
+    </section>
+  );
+}
+
+export default About;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <div>
+      <h1>Haley</h1>
+      <h2>I'm sorry</h2>
+    </div>
+  );
+}
+
+export default Header;
